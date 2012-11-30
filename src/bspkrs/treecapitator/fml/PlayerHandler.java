b1ce721b002170da597169521683ab9ca7e0f97d
bspkrs <bspkrs@gmail.com>
2012-11-29 23:15:03 -0500

    changed to PlayerInteractEvent for setting block hardness

--- src/bspkrs/treecapitator/fml/PlayerHandler.java
+++ src/bspkrs/treecapitator/fml/PlayerHandler.java
@@ -1,16 +1,25 @@
 package bspkrs.treecapitator.fml;
 
+import net.minecraft.src.Block;
 import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import bspkrs.treecapitator.TreeBlockBreaker;
 import bspkrs.treecapitator.TreeCapitator;
 
 public class PlayerHandler
 {
     @ForgeSubscribe
-    public void handleBreakSpeed(PlayerEvent.BreakSpeed event)
+    public void onBlockClicked(PlayerInteractEvent event)
     {
-        if (TreeCapitator.logClasses.contains(event.block.getClass()))
-            event.newSpeed = TreeBlockBreaker.getBlockHardness(event.entityPlayer);
+        if (event.action.equals(PlayerInteractEvent.Action.LEFT_CLICK_BLOCK))
+        {
+            int blockID = event.entityPlayer.worldObj.getBlockId(event.x, event.y, event.z);
+            if (blockID > 0)
+            {
+                Block block = Block.blocksList[blockID];
+                if (block != null && TreeCapitator.logClasses.contains(block.getClass()))
+                    block.setHardness(TreeBlockBreaker.getBlockHardness(event.entityPlayer));
+            }
+        }
     }
 }
