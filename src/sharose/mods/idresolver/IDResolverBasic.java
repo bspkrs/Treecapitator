dc005ba23cfdfc9eb1acaa95ffae1dfb178596a6
bspkrs <bspkrs@gmail.com>
2013-01-28 01:23:35 -0500

    dummy ID Resolver classes used for compilation

---
+++ src/sharose/mods/idresolver/IDResolverBasic.java
@@ -0,0 +1,12 @@
+package sharose.mods.idresolver;
+
+import java.util.Properties;
+
+/**
+ * @author bspkrs this is just a dummy for compilation
+ */
+public class IDResolverBasic
+{
+    protected static Properties knownIDs;
+
+}
---
+++ src/sharose/mods/idresolver/IDResolverMod.java
@@ -0,0 +1,12 @@
+package sharose.mods.idresolver;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * @author bspkrs this is just a dummy for compilation
+ */
+@Mod(name = "IDResolver", modid = "IDResolver", version = "1.4.5 - Update 1")
+public class IDResolverMod
+{   
+    
+}
