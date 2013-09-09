1bc0d6b34a8977009b995e39075e0a03141bc2df
bspkrs <bspkrs@gmail.com>
2013-09-09 13:07:21 -0400

    implemented itemIDBlacklist for ToolRegistry

--- src/bspkrs/treecapitator/ToolRegistry.java
+++ src/bspkrs/treecapitator/ToolRegistry.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import bspkrs.util.BlockID;
 import bspkrs.util.ItemID;
 import bspkrs.util.ListUtils;
 
@@ -29,6 +30,8 @@ public class ToolRegistry
     private List<ItemID> vanAxeList;
     private List<ItemID> vanShearsList;
     
+    private List<ItemID> blacklist;
+    
     protected ToolRegistry()
     {
         instance = this;
@@ -41,6 +44,7 @@ public class ToolRegistry
     {
         axeList = new ArrayList<ItemID>();
         shearsList = new ArrayList<ItemID>();
+        blacklist = new ArrayList<ItemID>();
     }
     
     protected void initVanillaLists()
@@ -61,27 +65,40 @@ public class ToolRegistry
         vanShearsList.add(new ItemID(Item.shears));
     }
     
+    public List<BlockID> blacklist()
+    {
+        return new ArrayList<BlockID>(blacklist);
+    }
+    
+    // This must be done after all trees are registered to avoid screwing up the registration process
+    public void readBlacklistFromDelimitedString(String dList)
+    {
+        blacklist = ListUtils.getDelimitedStringAsItemIDList(dList, ";");
+    }
+    
     protected void readFromNBT(NBTTagCompound ntc)
     {
         axeList = ListUtils.getDelimitedStringAsItemIDList(ntc.getString(Strings.AXE_ID_LIST), ";");
         shearsList = ListUtils.getDelimitedStringAsItemIDList(ntc.getString(Strings.SHEARS_ID_LIST), ";");
+        blacklist = ListUtils.getDelimitedStringAsItemIDList(ntc.getString(Strings.BLACKLIST), ";");
     }
     
     public void writeToNBT(NBTTagCompound ntc)
     {
         ntc.setString(Strings.AXE_ID_LIST, ListUtils.getListAsDelimitedString(axeList, ";"));
         ntc.setString(Strings.SHEARS_ID_LIST, ListUtils.getListAsDelimitedString(shearsList, ";"));
+        ntc.setString(Strings.BLACKLIST, ListUtils.getListAsDelimitedString(blacklist, ";"));
     }
     
     public void registerAxe(ItemID axe)
     {
-        if (axe != null && !axeList.contains(axe))
+        if (axe != null && !blacklist.contains(axe) && !axeList.contains(axe))
             axeList.add(axe);
     }
     
     public void registerShears(ItemID shears)
     {
-        if (shears != null && !shearsList.contains(shears))
+        if (shears != null && !blacklist.contains(shears) && !shearsList.contains(shears))
             shearsList.add(shears);
     }
     
@@ -107,34 +124,43 @@ public class ToolRegistry
     
     public boolean isAxe(ItemID itemID)
     {
-        return axeList.contains(itemID);
+        return !blacklist.contains(itemID) && axeList.contains(itemID);
     }
     
     public boolean isAxe(Item item)
     {
         if (item != null)
-            return axeList.contains(new ItemID(item));
-        else
-            return false;
+        {
+            ItemID itemID = new ItemID(item);
+            return !blacklist.contains(itemID) && axeList.contains(itemID);
+        }
+        
+        return false;
     }
     
     public boolean isAxe(ItemStack itemStack)
     {
         if (itemStack != null)
-            return axeList.contains(new ItemID(itemStack));
+        {
+            ItemID itemID = new ItemID(itemStack);
+            return !blacklist.contains(itemID) && axeList.contains(new ItemID(itemStack));
+        }
         else
             return false;
     }
     
     public boolean isShears(ItemID itemID)
     {
-        return shearsList.contains(itemID);
+        return !blacklist.contains(itemID) && shearsList.contains(itemID);
     }
     
     public boolean isShears(Item item)
     {
         if (item != null)
-            return shearsList.contains(new ItemID(item));
+        {
+            ItemID itemID = new ItemID(item);
+            return !blacklist.contains(itemID) && shearsList.contains(new ItemID(item));
+        }
         else
             return false;
     }
@@ -142,7 +168,10 @@ public class ToolRegistry
     public boolean isShears(ItemStack itemStack)
     {
         if (itemStack != null)
-            return shearsList.contains(new ItemID(itemStack));
+        {
+            ItemID itemID = new ItemID(itemStack);
+            return !blacklist.contains(itemID) && shearsList.contains(new ItemID(itemStack));
+        }
         else
             return false;
     }
