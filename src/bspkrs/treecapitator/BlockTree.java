74118bf7bc64476d3a4ef31faca460524fdad679
bspkrs <bspkrs@gmail.com>
2013-09-20 20:31:41 -0400

    1.6.4 update

--- src/bspkrs/treecapitator/BlockTree.java
+++ src/bspkrs/treecapitator/BlockTree.java
@@ -17,7 +17,7 @@ public class BlockTree extends BlockLog
         setHardness(TCSettings.logHardnessNormal);
         setStepSound(Block.soundWoodFootstep);
         setUnlocalizedName("log");
-        func_111022_d("log");
+        setTextureName("log");
     }
     
     /**
