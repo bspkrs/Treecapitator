918f6145da12cd98cb80bfe0106fc031ab191431
bspkrs <bspkrs@gmail.com>
2014-01-15 13:35:46 -0500

    renamed client ticker class

--- src/main/java/bspkrs/treecapitator/ClientProxy.java
+++ src/main/java/bspkrs/treecapitator/ClientProxy.java
@@ -16,7 +16,7 @@ public class ClientProxy extends CommonProxy
     public void init(FMLInitializationEvent event)
     {
         super.init(event);
-        new TreeCapitatorTicker();
+        new TCClientTicker();
     }
     
     @Override
--- src/main/java/bspkrs/treecapitator/TCClientTicker.java
+++ src/main/java/bspkrs/treecapitator/TCClientTicker.java
@@ -13,11 +13,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class TreeCapitatorTicker
+public class TCClientTicker
 {
     private Minecraft mcClient;
     
-    public TreeCapitatorTicker()
+    public TCClientTicker()
     {
         mcClient = FMLClientHandler.instance().getClient();
         FMLCommonHandler.instance().bus().register(this);
