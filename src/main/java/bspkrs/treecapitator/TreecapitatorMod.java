3d1adf32476fe6991e6a4e1737aded0b015e2037
bspkrs <bspkrs@gmail.com>
2014-05-16 02:15:35 -0400

    moved code to ForgeEventHandler and added break speed caching to improve performance

--- src/main/java/bspkrs/treecapitator/TreecapitatorMod.java
+++ src/main/java/bspkrs/treecapitator/TreecapitatorMod.java
@@ -2,9 +2,6 @@ package bspkrs.treecapitator;
 
 import java.io.File;
 
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import bspkrs.bspkrscore.fml.bspkrsCoreMod;
 import bspkrs.treecapitator.compat.MultiMineCompat;
@@ -14,14 +11,11 @@ import bspkrs.treecapitator.forge.ForgeEventHandler;
 import bspkrs.treecapitator.registry.ModConfigRegistry;
 import bspkrs.treecapitator.registry.RegistryNBTManager;
 import bspkrs.treecapitator.registry.ThirdPartyModConfig;
-import bspkrs.treecapitator.registry.TreeDefinition;
 import bspkrs.treecapitator.registry.TreeRegistry;
 import bspkrs.treecapitator.util.Reference;
 import bspkrs.treecapitator.util.TCLog;
-import bspkrs.util.BlockID;
 import bspkrs.util.CommonUtils;
 import bspkrs.util.Const;
-import bspkrs.util.Coord;
 import bspkrs.util.ModVersionChecker;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Loader;
@@ -42,7 +36,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class TreecapitatorMod
 {
     public static ModVersionChecker versionChecker;
-    private final String            versionURL = Const.VERSION_URL + "/Minecraft/" + Const.MCVERSION + "/treeCapitatorForge.version";
+    private final String            versionURL = Const.VERSION_URL + "/Minecraft/" + Const.MCVERSION + "/treecapitator.version";
     private final String            mcfTopic   = "http://www.minecraftforum.net/topic/1009577-";
     
     private RegistryNBTManager      nbtManager;
@@ -107,7 +101,7 @@ public class TreecapitatorMod
             if (msg.isNBTMessage())
             {
                 TCLog.info("Received IMC message from mod %s.", msg.getSender());
-                ModConfigRegistry.instance().registerIMCModConfig(msg.getSender(), new ThirdPartyModConfig(msg.getNBTValue()));
+                ModConfigRegistry.instance().registerIMCModConfig(msg.getSender(), ThirdPartyModConfig.readFromNBT(msg.getNBTValue()));
             }
             else
                 TCLog.warning("Mod %s send an IMC message, but it is not an NBT object message. The message will be ignored.", msg.getSender());
@@ -127,45 +121,6 @@ public class TreecapitatorMod
         
         // Make sure the NBT manager is initialized while we can still be sure of the values in our local objects
         nbtManager();
-    }
-    
-    public void onBlockHarvested(World world, int x, int y, int z, Block block, int metadata, EntityPlayer entityPlayer)
-    {
-        if (proxy.isEnabled() && !world.isRemote)
-        {
-            BlockID blockID = new BlockID(block, metadata);
-            
-            if (TreeRegistry.instance().isRegistered(blockID))
-            {
-                Coord blockPos = new Coord(x, y, z);
-                if (TreeRegistry.instance().trackTreeChopEventAt(blockPos))
-                {
-                    TCLog.debug("BlockID " + blockID + " is a log.");
-                    
-                    if (Treecapitator.isBreakingEnabled(entityPlayer) && Treecapitator.isBreakingPossible(world, entityPlayer, true))
-                    {
-                        TreeDefinition treeDef = TreeRegistry.instance().get(blockID);
-                        
-                        if (treeDef != null)
-                        {
-                            Treecapitator breaker = new Treecapitator(entityPlayer, treeDef);
-                            breaker.onBlockHarvested(world, x, y, z, metadata, entityPlayer);
-                        }
-                        else
-                            TCLog.severe("TreeRegistry reported block ID %s is a log, but TreeDefinition lookup failed! " +
-                                    "Please report this to bspkrs (include a copy of this log file and your config).", blockID);
-                        
-                    }
-                    else
-                        TCLog.debug("Chopping disabled due to player state or gamemode.");
-                    
-                    TreeRegistry.instance().endTreeChopEventAt(blockPos);
-                }
-                else
-                    TCLog.debug("Previous chopping event detected for block @%s", blockPos.toString());
-                
-            }
-        }
     }
     
     public RegistryNBTManager nbtManager()
--- src/main/java/bspkrs/treecapitator/forge/ForgeEventHandler.java
+++ src/main/java/bspkrs/treecapitator/forge/ForgeEventHandler.java
@@ -1,27 +1,41 @@
 package bspkrs.treecapitator.forge;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import bspkrs.helpers.block.BlockHelper;
 import bspkrs.helpers.entity.player.EntityPlayerHelper;
+import bspkrs.helpers.item.ItemHelper;
 import bspkrs.helpers.world.WorldHelper;
 import bspkrs.treecapitator.Treecapitator;
 import bspkrs.treecapitator.TreecapitatorMod;
+import bspkrs.treecapitator.config.TCConfigHandler;
 import bspkrs.treecapitator.config.TCSettings;
+import bspkrs.treecapitator.registry.ModConfigRegistry;
 import bspkrs.treecapitator.registry.TreeDefinition;
 import bspkrs.treecapitator.registry.TreeRegistry;
 import bspkrs.treecapitator.util.TCLog;
-import bspkrs.util.BlockID;
+import bspkrs.util.Coord;
+import bspkrs.util.ModulusBlockID;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler
 {
-    private Map<String, Boolean> playerSneakingMap = new HashMap<String, Boolean>();
+    private Map<String, Boolean>         playerSneakingMap = new HashMap<String, Boolean>();
+    private Map<CachedBreakSpeed, Float> breakSpeedCache   = new ConcurrentHashMap<CachedBreakSpeed, Float>(1000);
     
     @SubscribeEvent
     public void onBlockClicked(PlayerInteractEvent event)
@@ -29,55 +43,57 @@ public class ForgeEventHandler
         if (TreecapitatorMod.proxy.isEnabled() && event.action.equals(PlayerInteractEvent.Action.LEFT_CLICK_BLOCK))
         {
             if (!WorldHelper.isAirBlock(event.entityPlayer.worldObj, event.x, event.y, event.z))
-            {
-                Block block = WorldHelper.getBlock(event.entityPlayer.worldObj, event.x, event.y, event.z);
-                int metadata = event.entityPlayer.worldObj.getBlockMetadata(event.x, event.y, event.z);
-                
-                if (TCSettings.allowDebugOutput && event.entityPlayer.isSneaking() && block != null)
-                {
-                    TreecapitatorMod.proxy.debugOutputBlockID(BlockHelper.getUniqueID(block), metadata);
-                }
-                
                 playerSneakingMap.put(EntityPlayerHelper.getGameProfile(event.entityPlayer).getName(), event.entityPlayer.isSneaking());
-            }
         }
     }
     
     @SubscribeEvent
     public void getPlayerBreakSpeed(BreakSpeed event)
     {
-        BlockID blockID = new BlockID(event.block, event.metadata);
+        ModulusBlockID blockID = new ModulusBlockID(event.block, event.metadata, 4);
         
-        if (TreecapitatorMod.proxy.isEnabled() && TreeRegistry.instance().isRegistered(blockID) &&
-                (Treecapitator.isAxeItemEquipped(event.entityPlayer) || !TCSettings.needItem))
+        if (TreecapitatorMod.proxy.isEnabled() && (TreeRegistry.instance().isRegistered(blockID)
+                || (TCSettings.allowAutoTreeDetection && TreeRegistry.canAutoDetect(event.entityPlayer.worldObj, event.block, event.x, event.y, event.z)))
+                && Treecapitator.isBreakingPossible(event.entityPlayer, event.block, event.metadata, false))
         {
-            TreeDefinition treeDef = TreeRegistry.instance().get(blockID);
+            
+            TreeDefinition treeDef;
+            if (TCSettings.allowAutoTreeDetection)
+                treeDef = TreeRegistry.autoDetectTree(event.entityPlayer.worldObj, blockID, new Coord(event.x, event.y, event.z), TCSettings.allowDebugLogging);
+            else
+                treeDef = TreeRegistry.instance().get(blockID);
+            
             if (treeDef != null)
             {
-                if (TCSettings.treeHeightDecidesBreakSpeed)
+                boolean swappedSneak = !((playerSneakingMap.containsKey(EntityPlayerHelper.getGameProfile(event.entityPlayer).getName())
+                        && (playerSneakingMap.get(EntityPlayerHelper.getGameProfile(event.entityPlayer).getName()) == event.entityPlayer.isSneaking()))
+                        || !playerSneakingMap.containsKey(EntityPlayerHelper.getGameProfile(event.entityPlayer).getName()));
+                
+                CachedBreakSpeed c = new CachedBreakSpeed(event, swappedSneak);
+                if (!this.breakSpeedCache.containsKey(c))
                 {
-                    if ((playerSneakingMap.containsKey(EntityPlayerHelper.getGameProfile(event.entityPlayer))
-                            && (playerSneakingMap.get(EntityPlayerHelper.getGameProfile(event.entityPlayer)) == event.entityPlayer.isSneaking()))
-                            || !playerSneakingMap.containsKey(EntityPlayerHelper.getGameProfile(event.entityPlayer)))
+                    if (!swappedSneak)
                     {
-                        if (Treecapitator.isBreakingEnabled(event.entityPlayer))
+                        if (TCSettings.treeHeightDecidesBreakSpeed)
                         {
-                            int height = Treecapitator.getTreeHeight(treeDef, event.entityPlayer.worldObj, event.x, event.y, event.z, event.metadata, event.entityPlayer);
-                            if (height > 1)
-                                event.newSpeed = event.originalSpeed / (height * TCSettings.treeHeightModifier);
+                            if (Treecapitator.isBreakingEnabled(event.entityPlayer))
+                            {
+                                int height = Treecapitator.getTreeHeight(treeDef, event.entityPlayer.worldObj, event.x, event.y, event.z, event.metadata, event.entityPlayer);
+                                if (height > 1)
+                                    event.newSpeed = event.originalSpeed / (height * TCSettings.treeHeightModifier);
+                            }
                         }
+                        else if (Treecapitator.isBreakingEnabled(event.entityPlayer))
+                            event.newSpeed = event.originalSpeed * treeDef.breakSpeedModifier();
                     }
                     else
-                    {
-                        event.newSpeed = 0.0000000001f;
-                    }
+                        event.newSpeed = 0.0f;
+                    
+                    this.breakSpeedCache.put(c, event.newSpeed);
                 }
-                else if (Treecapitator.isBreakingEnabled(event.entityPlayer))
-                    event.newSpeed = event.originalSpeed * treeDef.breakSpeedModifier();
+                else
+                    event.newSpeed = this.breakSpeedCache.get(c);
             }
-            else
-                TCLog.severe("TreeRegistry reported block ID %s is a log, but TreeDefinition lookup failed! " +
-                        "Please report this to bspkrs (include a copy of this log file and your config).", blockID);
         }
     }
     
@@ -85,6 +101,102 @@ public class ForgeEventHandler
     public void onBlockHarvested(BreakEvent event)
     {
         if (event.block != null && event.world != null && event.getPlayer() != null)
-            TreecapitatorMod.instance.onBlockHarvested(event.world, event.x, event.y, event.z, event.block, event.blockMetadata, event.getPlayer());
+        {
+            List<CachedBreakSpeed> toRemove = new ArrayList<CachedBreakSpeed>();
+            for (CachedBreakSpeed bs : this.breakSpeedCache.keySet())
+                if (bs.entityPlayer.getGameProfile().getName().equals(event.getPlayer().getGameProfile().getName()))
+                    toRemove.add(bs);
+            
+            for (CachedBreakSpeed bs : toRemove)
+                this.breakSpeedCache.remove(bs);
+            
+            if (playerSneakingMap.containsKey(event.getPlayer().getGameProfile().getName()))
+                playerSneakingMap.remove(event.getPlayer().getGameProfile().getName());
+            
+            if (TreecapitatorMod.proxy.isEnabled() && !event.world.isRemote)
+            {
+                ModulusBlockID blockID = new ModulusBlockID(event.block, event.blockMetadata, 4);
+                
+                if (((TCSettings.allowAutoTreeDetection && TreeRegistry.canAutoDetect(event.world, event.block, event.x, event.y, event.z)) || TreeRegistry.instance().isRegistered(blockID))
+                        && Treecapitator.isBreakingPossible(event.getPlayer(), event.block, event.blockMetadata, TCSettings.allowDebugLogging))
+                {
+                    Coord blockPos = new Coord(event.x, event.y, event.z);
+                    if (TreeRegistry.instance().trackTreeChopEventAt(blockPos))
+                    {
+                        TCLog.debug("BlockID " + blockID + " is a log.");
+                        
+                        TreeDefinition treeDef;
+                        if (TCSettings.allowAutoTreeDetection)
+                            treeDef = TreeRegistry.autoDetectTree(event.world, blockID, blockPos, TCSettings.allowDebugLogging);
+                        else
+                            treeDef = TreeRegistry.instance().get(blockID);
+                        
+                        if (treeDef != null)
+                            (new Treecapitator(event.getPlayer(), treeDef)).onBlockHarvested(event.world, event.x, event.y, event.z, event.blockMetadata);
+                        
+                        TreeRegistry.instance().endTreeChopEventAt(blockPos);
+                    }
+                    else
+                        TCLog.debug("Previous chopping event detected for block @%s", blockPos.toString());
+                }
+            }
+            
+            if (ModConfigRegistry.instance().isChanged())
+                ModConfigRegistry.instance().writeChangesToConfig(TCConfigHandler.instance().getConfig());
+        }
+    }
+    
+    private class CachedBreakSpeed extends BreakSpeed
+    {
+        private boolean isSneaking;
+        private boolean swappedSneak;
+        
+        public CachedBreakSpeed(BreakSpeed event, boolean swappedSneak)
+        {
+            super(event.entityPlayer, event.block, event.metadata, event.originalSpeed, event.x, event.y, event.z);
+            this.isSneaking = event.entityPlayer.isSneaking();
+            this.swappedSneak = swappedSneak;
+        }
+        
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o)
+                return true;
+            
+            if (!(o instanceof CachedBreakSpeed))
+                return false;
+            
+            CachedBreakSpeed bs = (CachedBreakSpeed) o;
+            
+            ItemStack oItem = bs.entityPlayer.getCurrentEquippedItem();
+            ItemStack thisItem = this.entityPlayer.getCurrentEquippedItem();
+            
+            return bs.entityPlayer.equals(this.entityPlayer) && (oItem != null ? (thisItem != null ? thisItem.isItemEqual(oItem) : false) : thisItem == null)
+                    && BlockHelper.getUniqueID(bs.block).equals(BlockHelper.getUniqueID(this.block))
+                    && bs.isSneaking == this.isSneaking && bs.swappedSneak == this.swappedSneak
+                    && bs.metadata == this.metadata && bs.originalSpeed == this.originalSpeed && bs.x == this.x && bs.y == this.y && bs.z == this.z;
+        }
+        
+        @Override
+        public int hashCode()
+        {
+            ItemStack thisItem = this.entityPlayer.getCurrentEquippedItem();
+            HashFunction hf = Hashing.md5();
+            Hasher h = hf.newHasher()
+                    .putString(EntityPlayerHelper.getGameProfile(this.entityPlayer).getName(), Charsets.UTF_8)
+                    .putString(BlockHelper.getUniqueID(this.block), Charsets.UTF_8)
+                    .putBoolean(this.isSneaking)
+                    .putBoolean(this.swappedSneak)
+                    .putInt(this.metadata)
+                    .putFloat(this.originalSpeed)
+                    .putInt(x + z << 8 + y << 16);
+            
+            if (thisItem != null)
+                h.putString(ItemHelper.getUniqueID(thisItem.getItem()), Charsets.UTF_8)
+                        .putInt(thisItem.getItemDamage());
+            
+            return h.hash().hashCode();
+        }
     }
 }
