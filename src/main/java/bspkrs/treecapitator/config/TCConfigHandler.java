19b8c2386328132a76b8b10d8e6c7e4dfb12eb98
bspkrs <bspkrs@gmail.com>
2014-05-16 02:16:26 -0400

    added config versioning and error handling

--- src/main/java/bspkrs/treecapitator/config/TCConfigHandler.java
+++ src/main/java/bspkrs/treecapitator/config/TCConfigHandler.java
@@ -1,12 +1,13 @@
 package bspkrs.treecapitator.config;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import bspkrs.treecapitator.TreecapitatorMod;
 import bspkrs.treecapitator.registry.ModConfigRegistry;
 import bspkrs.treecapitator.util.Reference;
 import bspkrs.treecapitator.util.TCLog;
-import bspkrs.util.CommonUtils;
 import bspkrs.util.config.ConfigChangedEvent.OnConfigChangedEvent;
 import bspkrs.util.config.Configuration;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +15,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class TCConfigHandler
 {
     private static TCConfigHandler instance;
+    private File                   fileRef;
     private Configuration          config;
     private boolean                shouldRefreshRegistries = false;
     
@@ -39,9 +41,32 @@ public class TCConfigHandler
     private TCConfigHandler(File file)
     {
         this();
-        config = new Configuration(file);
-        TCLog.info("Loading configuration file %s", file.getAbsolutePath().replace(CommonUtils.getMinecraftDir(), "./"));
-        syncConfig();
+        this.fileRef = file;
+        TCLog.info("Loading configuration file %s", file.getAbsolutePath());
+        try
+        {
+            config = new Configuration(fileRef, Reference.CONFIG_VERSION);
+        }
+        catch (Throwable e)
+        {
+            File fileBak = new File(fileRef.getAbsolutePath() + "_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".errored");
+            TCLog.severe("An exception occurred while loading your config file. This file will be renamed to %s and a new config file will be generated.", fileBak.getName());
+            e.printStackTrace();
+            
+            fileRef.renameTo(fileBak);
+            config = new Configuration(fileRef, Reference.CONFIG_VERSION);
+        }
+        
+        if (!Reference.CONFIG_VERSION.equals(config.getLoadedConfigVersion()))
+        {
+            File fileBak = new File(fileRef.getAbsolutePath() + "_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".old");
+            TCLog.warning("Your Treecapitator config file is out of date and could cause issues. The existing file will be renamed to %s and a new one will be generated.", fileBak.getName());
+            
+            fileRef.renameTo(fileBak);
+            config = new Configuration(fileRef, Reference.CONFIG_VERSION);
+        }
+        
+        syncConfig(true);
     }
     
     public Configuration getConfig()
@@ -56,10 +81,29 @@ public class TCConfigHandler
     
     public void syncConfig()
     {
-        config.load();
+        syncConfig(false);
+    }
+    
+    public void syncConfig(boolean init)
+    {
+        if (!init)
+            try
+            {
+                config.load();
+            }
+            catch (Throwable e)
+            {
+                File fileBak = new File(fileRef.getAbsolutePath() + "_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".errored");
+                TCLog.severe("An exception occurred while loading your config file. This file will be renamed to %s and a new config file will be generated.", fileBak.getName());
+                e.printStackTrace();
+                
+                fileRef.renameTo(fileBak);
+                
+                config = new Configuration(fileRef, Reference.CONFIG_VERSION);
+            }
         
         TCSettings.instance().syncConfiguration(config);
-        ModConfigRegistry.instance().syncConfiguration(config);
+        ModConfigRegistry.instance().syncConfig(config);
         
         if (this.shouldRefreshRegistries)
         {
